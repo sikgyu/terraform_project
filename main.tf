3c1f93d9191127ef1352aabd44dbf868e75e31c7
sikgyu <57619483+sikgyu@users.noreply.github.com>
2022-07-05 17:17:20 -0700

    :art: Create a new ec2 instance

--- main.tf
+++ main.tf
@@ -20,3 +20,25 @@ module "my_vpc" {
   public_sub_1_cidr  = "10.0.1.0/24"
   private_sub_1_cidr = "10.0.2.0/24"
 }
+
+data "aws_ami" "amz_linux_2" {
+  most_recent = true
+  name_regex  = "amzn2-ami-hvm-2.*.1-x86_64-gp2"
+  owners      = ["amazon"]
+}
+
+resource "aws_instance" "web_instance" {
+  ami           = data.aws_ami.amz_linux_2.id
+  instance_type = "t2.nano"
+
+  subnet_id                   = ?
+  vpc_security_group_ids      = ?
+  associate_public_ip_address = true
+
+  user_data = <<-EOF
+  #!/bin/bash -ex
+  amazon-linux-extras install nginx1 -y
+  systemctl enable nginx
+  systemctl start nginx
+  EOF
+}
