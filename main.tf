f0b4d607bb4d89a03a9e2c40b4a79f083db2ee43
sikgyu <57619483+sikgyu@users.noreply.github.com>
2022-07-05 17:16:29 -0700

    :art: Dynamic CIDR blocks using variables

--- main.tf
+++ main.tf
@@ -15,4 +15,8 @@ provider "aws" {
 
 module "my_vpc" {
   source = "./modules/vpc"
+
+  vpc_cidr           = "10.0.0.0/16"
+  public_sub_1_cidr  = "10.0.1.0/24"
+  private_sub_1_cidr = "10.0.2.0/24"
 }
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,38 +1,38 @@
-resource "aws_vpc" "some_custom_vpc" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "custom_vpc" {
+  cidr_block = var.vpc_cidr
 }
 
-resource "aws_subnet" "some_public_subnet" {
-  vpc_id            = aws_vpc.some_custom_vpc.id
-  cidr_block        = "10.0.1.0/24"
+resource "aws_subnet" "custom_public_subnet" {
+  vpc_id            = aws_vpc.custom_vpc.id
+  cidr_block        = var.public_sub_1_cidr
 }
 
-resource "aws_subnet" "some_private_subnet" {
-  vpc_id            = aws_vpc.some_custom_vpc.id
-  cidr_block        = "10.0.2.0/24"
+resource "aws_subnet" "custom_private_subnet" {
+  vpc_id            = aws_vpc.custom_vpc.id
+  cidr_block        = var.private_sub_1_cidr
 }
 
-resource "aws_internet_gateway" "some_ig" {
-  vpc_id = aws_vpc.some_custom_vpc.id
+resource "aws_internet_gateway" "custom_ig" {
+  vpc_id = aws_vpc.custom_vpc.id
 }
 
 resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.some_custom_vpc.id
+  vpc_id = aws_vpc.custom_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.some_ig.id
+    gateway_id = aws_internet_gateway.custom_ig.id
   }
 }
 
 resource "aws_route_table_association" "public_1_rt_a" {
-  subnet_id      = aws_subnet.some_public_subnet.id
+  subnet_id      = aws_subnet.custom_public_subnet.id
   route_table_id = aws_route_table.public_rt.id
 }
 
 resource "aws_security_group" "web_sg" {
   name   = "HTTP and SSH"
-  vpc_id = aws_vpc.some_custom_vpc.id
+  vpc_id = aws_vpc.custom_vpc.id
 
   ingress {
     from_port   = 80
